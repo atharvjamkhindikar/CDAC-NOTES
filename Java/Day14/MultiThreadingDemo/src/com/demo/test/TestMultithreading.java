5f35f4e268336b69638ae46cde8231759f10cdc9
Atharv Jamkhindikar <148607578+atharvjamkhindikar@users.noreply.github.com>
2025-12-10 18:32:53 +0530

    Java Notes Day 14
    Java Notes

---
+++ Java/Day14/MultiThreadingDemo/src/com/demo/test/TestMultithreading.java
@@ -0,0 +1,50 @@
+package com.demo.test;
+
+import com.demo.beans.MyClass;
+import com.demo.threads.MyFactorialThread;
+import com.demo.threads.MyPrimeThread;
+import com.demo.threads.MyPrintTableThread;
+import com.demo.threads.OddAndEvenThread;
+
+public class TestMultithreading {
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+		//using Thread
+		MyFactorialThread fth=new MyFactorialThread(5,ob);
+		fth.start();
+		
+		MyPrintTableThread th1=new MyPrintTableThread(3, ob);
+		th1.start();
+		
+		//using Thread
+		MyPrintTableThread th2=new MyPrintTableThread(7, ob);
+		th2.start();
+		
+		//using Runnable object
+		MyPrimeThread th3=new MyPrimeThread(ob, 9);
+		Thread th=new Thread(th3);
+		th.start();
+		
+		//using Thread
+		int arr [] = {10, 15, 20, 25};
+		OddAndEvenThread th4 = new OddAndEvenThread(ob, arr);
+		th4.start();
+		
+		try {
+			fth.join();
+			th1.join();
+			th2.join();
+			th.join();
+			th4.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println("main function continues....");
+
+
+	}
+
+}
