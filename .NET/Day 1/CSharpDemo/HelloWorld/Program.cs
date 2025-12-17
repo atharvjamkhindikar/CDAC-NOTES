f25dbcc1c0c8827771f512fd02da46e742f249a7
Atharv Jamkhindikar <148607578+atharvjamkhindikar@users.noreply.github.com>
2025-12-17 19:38:41 +0530

    Day 1 Notes
    .NET Notes of CDAC

---
+++ .NET/Day 1/CSharpDemo/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace HelloWorld
+{
+    internal class Program
+    {
+           
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To The .Net World AJ...!");
+            Program.Addition(5,10);
+            Console.WriteLine(Addition(5,10));
+        }
+
+        private static int Addition(int x,int y)
+        {
+             return x + y;
+             
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ .NET/Day 1/CSharpDemo/Prime Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Prime_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Prime Numbers ");
+
+            Console.WriteLine("Inter num to check its Prime or not ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if(CheckPrimes(x))
+            {
+                Console.WriteLine(x + " is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine(x +" is not a Prime Number");
+            }
+
+        }
+        private static bool CheckPrimes(int x)
+        {
+            if (x <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
+
+
