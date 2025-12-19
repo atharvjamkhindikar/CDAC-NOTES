9eb42b73c6fc2154e2e6235b419332fbccc76f92
Atharv Jamkhindikar <148607578+atharvjamkhindikar@users.noreply.github.com>
2025-12-19 19:04:16 +0530

    Day 5 Notes
    .NET Notes

---
+++ .NET/Day 5/CSharpDemos/Swapping Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Swapping_Demo
+{
+    internal class Math
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Swapping of Numbers via Debug Mode----");
+            Math m1= new Math();
+            int a = 10, b = 20;
+            Console.WriteLine($"Before Swapping: a = {a}, b = {b}");
+            m1.swap(ref a,ref b);  
+            Console.WriteLine($"After Swapping: a = {a}, b = {b}");
+        }
+
+        public void swap(ref int x, ref int y)     // pass by reference
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
